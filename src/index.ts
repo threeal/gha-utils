4419fa252432ac897f214025c8a566976bb3ecf4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-08 13:28:06 +0700

    feat: add `setOutput` function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import fs from "node:fs";
+import os from "node:os";
+
 /**
  * Retrieves the value of a GitHub Actions input.
  *
@@ -7,4 +10,17 @@
 export function getInput(name: string): string {
   const value = process.env[`INPUT_${name.toUpperCase()}`] || "";
   return value.trim();
+}
+
+/**
+ * Sets the value of a GitHub Actions output.
+ *
+ * @param name - The name of the GitHub Actions output.
+ * @param value - The value of the GitHub Actions output
+ */
+export function setOutput(name: string, value: string): void {
+  fs.appendFileSync(
+    process.env["GITHUB_OUTPUT"] as string,
+    `${name}=${value}${os.EOL}`,
+  );
 }
