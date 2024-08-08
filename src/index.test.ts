20bfec505dfd012764afb252a853d0a60c38846e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-08 13:28:06 +0700

    test: add test for `setOutput` function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,7 @@
-import { getInput } from "./index.js";
+import fs from "node:fs";
+import os from "node:os";
+import path from "node:path";
+import { getInput, setOutput } from "./index.js";
 
 describe("retrieve GitHub Actions inputs", () => {
   it("should retrieve a GitHub Actions input", () => {
@@ -8,5 +11,31 @@ describe("retrieve GitHub Actions inputs", () => {
 
   it("should retrieve an empty GitHub Actions input", () => {
     expect(getInput("some-empty-input")).toBe("");
+  });
+});
+
+describe("set GitHub Actions outputs", () => {
+  let tempFile: string;
+  beforeAll(() => {
+    tempFile = path.join(os.tmpdir(), "output");
+    process.env["GITHUB_OUTPUT"] = tempFile;
+    if (fs.existsSync(tempFile)) {
+      fs.rmSync(tempFile);
+    }
+  });
+
+  it("should set GitHub Actions outputs", () => {
+    setOutput("some-output", "some value");
+    setOutput("some-other-output", "some other value");
+
+    expect(fs.readFileSync(tempFile, { encoding: "utf-8" })).toBe(
+      `some-output=some value${os.EOL}some-other-output=some other value${os.EOL}`,
+    );
+  });
+
+  afterAll(() => {
+    if (fs.existsSync(tempFile)) {
+      fs.rmSync(tempFile);
+    }
   });
 });
