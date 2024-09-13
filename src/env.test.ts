5f6a8792a529ade14e78b0d08871f1eb97688497
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-13 19:09:18 +0700

    refactor: add `env.ts` separated from `index.ts`

---
+++ src/env.test.ts
@@ -0,0 +1,189 @@
+import fsPromises from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+
+import {
+  addPath,
+  addPathSync,
+  getInput,
+  mustGetEnvironment,
+  setEnv,
+  setEnvSync,
+  setOutput,
+  setOutputSync,
+} from "./env.js";
+
+describe("retrieve environment variables", () => {
+  it("should retrieve an environment variable", () => {
+    process.env["AN_ENV"] = "a value";
+    expect(mustGetEnvironment("AN_ENV")).toBe("a value");
+  });
+
+  it("should fail to retrieve an undefined environment variable", () => {
+    delete process.env["AN_UNDEFINED_ENV"];
+    expect(() => mustGetEnvironment("AN_UNDEFINED_ENV")).toThrow(
+      "the AN_UNDEFINED_ENV environment variable must be defined",
+    );
+  });
+});
+
+describe("retrieve GitHub Actions inputs", () => {
+  it("should retrieve a GitHub Actions input", () => {
+    process.env["INPUT_AN-INPUT"] = " a value  ";
+    expect(getInput("an-input")).toBe("a value");
+  });
+
+  it("should retrieve an undefined GitHub Actions input", () => {
+    delete process.env["INPUT_AN-UNDEFINED-INPUT"];
+    expect(getInput("an-undefined-input")).toBe("");
+  });
+});
+
+describe("set GitHub Actions outputs", () => {
+  let tempFile: string;
+  beforeEach(async () => {
+    tempFile = path.join(os.tmpdir(), "output");
+    process.env["GITHUB_OUTPUT"] = tempFile;
+    try {
+      await fsPromises.rm(tempFile);
+    } catch (err) {
+      if ((err as any).code !== "ENOENT") throw err;
+    }
+  });
+
+  it("should set GitHub Actions outputs", async () => {
+    await Promise.all([
+      setOutput("an-output", "a value"),
+      setOutput("another-output", "another value"),
+    ]);
+
+    const lines = (await fsPromises.readFile(tempFile, { encoding: "utf-8" }))
+      .split(os.EOL)
+      .filter((line) => line !== "")
+      .sort();
+
+    expect(lines).toEqual([
+      "an-output=a value",
+      "another-output=another value",
+    ]);
+  });
+
+  it("should set GitHub Actions outputs synchronously", async () => {
+    setOutputSync("an-output", "a value");
+    setOutputSync("another-output", "another value");
+
+    const content = await fsPromises.readFile(tempFile, { encoding: "utf-8" });
+    expect(content).toBe(
+      `an-output=a value${os.EOL}another-output=another value${os.EOL}`,
+    );
+  });
+
+  afterEach(async () => {
+    try {
+      await fsPromises.rm(tempFile);
+    } catch (err) {
+      if ((err as any).code !== "ENOENT") throw err;
+    }
+  });
+});
+
+describe("set environment variables in GitHub Actions", () => {
+  let tempFile: string;
+  beforeEach(async () => {
+    tempFile = path.join(os.tmpdir(), "env");
+    process.env["GITHUB_ENV"] = tempFile;
+    try {
+      await fsPromises.rm(tempFile);
+    } catch (err) {
+      if ((err as any).code !== "ENOENT") throw err;
+    }
+  });
+
+  it("should set environment variables in GitHub Actions", async () => {
+    await Promise.all([
+      setEnv("AN_ENV", "a value"),
+      setEnv("ANOTHER_ENV", "another value"),
+    ]);
+
+    expect(process.env.AN_ENV).toBe("a value");
+    expect(process.env.ANOTHER_ENV).toBe("another value");
+
+    const lines = (await fsPromises.readFile(tempFile, { encoding: "utf-8" }))
+      .split(os.EOL)
+      .filter((line) => line !== "")
+      .sort();
+
+    expect(lines).toEqual(["ANOTHER_ENV=another value", "AN_ENV=a value"]);
+  });
+
+  it("should set environment variables in GitHub Actions synchronously", async () => {
+    setEnvSync("AN_ENV", "a value");
+    setEnvSync("ANOTHER_ENV", "another value");
+
+    expect(process.env.AN_ENV).toBe("a value");
+    expect(process.env.ANOTHER_ENV).toBe("another value");
+
+    const content = await fsPromises.readFile(tempFile, { encoding: "utf-8" });
+    expect(content).toBe(
+      `AN_ENV=a value${os.EOL}ANOTHER_ENV=another value${os.EOL}`,
+    );
+  });
+
+  afterEach(async () => {
+    try {
+      await fsPromises.rm(tempFile);
+    } catch (err) {
+      if ((err as any).code !== "ENOENT") throw err;
+    }
+  });
+});
+
+describe("adds system paths in GitHub Actions", () => {
+  let tempFile: string;
+  beforeEach(async () => {
+    tempFile = path.join(os.tmpdir(), "path");
+    process.env["GITHUB_PATH"] = tempFile;
+    try {
+      await fsPromises.rm(tempFile);
+    } catch (err) {
+      if ((err as any).code !== "ENOENT") throw err;
+    }
+  });
+
+  it("should add system paths in GitHub Actions", async () => {
+    await Promise.all([addPath("a-path"), addPath("another-path")]);
+
+    const sysPaths = (process.env["PATH"] ?? "")
+      .split(path.delimiter)
+      .slice(0, 2)
+      .sort();
+    expect(sysPaths).toEqual(["a-path", "another-path"]);
+
+    const lines = (await fsPromises.readFile(tempFile, { encoding: "utf-8" }))
+      .split(os.EOL)
+      .filter((line) => line !== "")
+      .sort();
+    expect(lines).toEqual(["a-path", "another-path"]);
+  });
+
+  it("should add system paths in GitHub Actions synchronously", async () => {
+    addPathSync("a-path");
+    addPathSync("another-path");
+
+    const sysPaths = (process.env["PATH"] ?? "")
+      .split(path.delimiter)
+      .slice(0, 2);
+    expect(sysPaths).toEqual(["another-path", "a-path"]);
+
+    const content = await fsPromises.readFile(tempFile, { encoding: "utf-8" });
+    expect(content).toBe(`a-path${os.EOL}another-path${os.EOL}`);
+  });
+
+  afterEach(async () => {
+    try {
+      await fsPromises.rm(tempFile);
+    } catch (err) {
+      if ((err as any).code !== "ENOENT") throw err;
+    }
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,14 @@
-import fs from "node:fs";
-import fsPromises from "node:fs/promises";
 import os from "node:os";
-import path from "node:path";
 
-/**
- * Retrieves the value of an environment variable.
- *
- * @param name - The name of the environment variable.
- * @returns The value of the environment variable.
- * @throws Error if the environment variable is not defined.
- */
-export function mustGetEnvironment(name: string): string {
-  const value = process.env[name];
-  if (value === undefined) {
-    throw new Error(`the ${name} environment variable must be defined`);
-  }
-  return value;
-}
-
-/**
- * Retrieves the value of a GitHub Actions input.
- *
- * @param name - The name of the GitHub Actions input.
- * @returns The value of the GitHub Actions input, or an empty string if not found.
- */
-export function getInput(name: string): string {
-  const value = process.env[`INPUT_${name.toUpperCase()}`] || "";
-  return value.trim();
-}
-
-/**
- * Sets the value of a GitHub Actions output.
- *
- * @param name - The name of the GitHub Actions output.
- * @param value - The value to set for the GitHub Actions output.
- * @returns A promise that resolves when the value is successfully set.
- */
-export async function setOutput(name: string, value: string): Promise<void> {
-  const filePath = mustGetEnvironment("GITHUB_OUTPUT");
-  await fsPromises.appendFile(filePath, `${name}=${value}${os.EOL}`);
-}
-
-/**
- * Sets the value of a GitHub Actions output synchronously.
- *
- * @param name - The name of the GitHub Actions output.
- * @param value - The value to set for the GitHub Actions output.
- */
-export function setOutputSync(name: string, value: string): void {
-  const filePath = mustGetEnvironment("GITHUB_OUTPUT");
-  fs.appendFileSync(filePath, `${name}=${value}${os.EOL}`);
-}
-
-/**
- * Sets the value of an environment variable in GitHub Actions.
- *
- * @param name - The name of the environment variable.
- * @param value - The value to set for the environment variable.
- * @returns A promise that resolves when the environment variable is
- *          successfully set.
- */
-export async function setEnv(name: string, value: string): Promise<void> {
-  process.env[name] = value;
-  const filePath = mustGetEnvironment("GITHUB_ENV");
-  await fsPromises.appendFile(filePath, `${name}=${value}${os.EOL}`);
-}
-
-/**
- * Sets the value of an environment variable in GitHub Actions synchronously.
- *
- * @param name - The name of the environment variable.
- * @param value - The value to set for the environment variable.
- */
-export function setEnvSync(name: string, value: string): void {
-  process.env[name] = value;
-  const filePath = mustGetEnvironment("GITHUB_ENV");
-  fs.appendFileSync(filePath, `${name}=${value}${os.EOL}`);
-}
-
-/**
- * Adds a system path to the environment in GitHub Actions.
- *
- * @param sysPath - The system path to add to the environment.
- * @returns A promise that resolves when the system path is successfully added.
- */
-export async function addPath(sysPath: string): Promise<void> {
-  process.env["PATH"] = `${sysPath}${path.delimiter}${process.env["PATH"]}`;
-  const filePath = mustGetEnvironment("GITHUB_PATH");
-  await fsPromises.appendFile(filePath, `${sysPath}${os.EOL}`);
-}
-
-/**
- * Adds a system path to the environment in GitHub Actions synchronously.
- *
- * @param sysPath - The system path to add to the environment.
- */
-export function addPathSync(sysPath: string): void {
-  process.env["PATH"] = `${sysPath}${path.delimiter}${process.env["PATH"]}`;
-  const filePath = mustGetEnvironment("GITHUB_PATH");
-  fs.appendFileSync(filePath, `${sysPath}${os.EOL}`);
-}
+export {
+  addPath,
+  addPathSync,
+  getInput,
+  setEnv,
+  setEnvSync,
+  setOutput,
+  setOutputSync,
+} from "./env.js";
 
 /**
  * Logs an information message in GitHub Actions.
