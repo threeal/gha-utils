8912d4c7e364bd2e7fd3b7b4a6e68e68809aec46
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-13 17:56:23 +0700

    feat: modify `logCommand` function to require variadic arguments (#82)

--- src/index.ts
+++ src/index.ts
@@ -132,7 +132,7 @@ export function logError(err: unknown): void {
  * @param command - The command to log.
  * @param args - The arguments of the command.
  */
-export function logCommand(command: string, args: readonly string[]): void {
+export function logCommand(command: string, ...args: string[]): void {
   const message = [command, ...args].join(" ");
   process.stdout.write(`[command]${message}${os.EOL}`);
 }
