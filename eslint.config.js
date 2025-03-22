616295f434ce16c4f54ef7ec33033e5bc7a08e50
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-03-22 21:11:41 +0700

    test: remove `.*` files from being ignored by ESLint
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,7 @@ export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.stylistic,
-  {
-    ignores: [".*", "dist", "docs"],
-  },
+  { ignores: ["dist", "docs"] },
   {
     files: ["**/*.test.ts"],
     rules: {
